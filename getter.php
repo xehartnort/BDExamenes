6e6608813e8faca9c3e5eacf95b3b2ddd1861447
Daniel Jimenez <xehartnort@gmail.com>
2016-06-26 12:18:05 +0200

    Formularios dinámicos en SQL funcionalesgit status

--- getter.php
+++ getter.php
@@ -1,4 +1,7 @@
 <?php
+
+
+// IDEA: para evitar las inyecciones de código sql, utilizar un usuario cuyos privilegios estén restringidos a lectura de la tabla exámenes
 $servername = "localhost";
 $username = "root";
 $password = "root";
@@ -14,24 +17,30 @@ if (mysqli_connect_error()) {
                           
 $sql = "select * from examenes where";
 
-if($_GET['grado']!="Pordefecto")
-  $sql .= " grado=\"".$_GET['grado']."\"";
+if($_GET['grado']!="null")
+  $sql .= " grado=\"".$_GET['grado']."\" and";
   
-if($_GET['curso']!="Pordefecto")
-  $sql .= " curso=".$_GET['curso'];
+if($_GET['curso']!="null")
+  $sql .= " curso=".$_GET['curso']." and";
 
-if($_GET['asignatura']!="Pordefecto")
-  $sql .= " asignatura=\"".$_GET['asignatura']."\"";
+if($_GET['asig']!="null")
+  $sql .= " asignatura=\"".$_GET['asig']."\" and";
 
-if($_GET['profesor']!="Pordefecto")
-  $sql .= " profesor=\"".$_GET['profesor']."\"";
+if($_GET['prof']!="null")
+  $sql .= " profesor=\"".$_GET['prof']."\" and";
 
-echo $sql;
+// si los tres últimos carácteres son un "and", los quitamos
+if( substr($sql, -3) == "and" )
+  $sql = substr($sql, 0, -3);
 
 $result = $conn->query($sql);
 
 while($row = $result->fetch_assoc()) {
   echo "<option>".$row[$_GET['caller']]."</option>";
 }
+
+// cerramos la conexión
+$conn->close();
+
     
 ?>
