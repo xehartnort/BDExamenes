09aa6e92d60b991bb9bb568cbf162202f11ee17e
xehartnort <xehartnort@gmail.com>
2016-11-06 17:20:47 +0100

    $.ajax --> $.getJSON

--- selectUpdater.js
+++ selectUpdater.js
@@ -4,49 +4,36 @@ function mostrarResultados(){
           "tag2="+$("#curso").val()+"&"+
           "tag3="+$("#asig").val()+"&"+
           "tag4="+$("#anio").val());
-  $.ajax({
-     type: 'GET',
-     url: 'getter.php',
-     data: args,
-     dataType: 'json',
-     success: function (data) {
-       $('#lista').empty();
-       //console.log(data);
-       if(data == null){
-         $('#lista').append('<li>'+'No se encontraron resultados'+'</li>');
-       }else{
-         $.each(data, function(key, value) {
-           var path2file = encodeURI(value+"/"+key);
-           $('#lista').append('<li><a href='+path2file+'>'+key+'</a></li>');
-         });
-       }
-     }
+  $.getJSON( 'getter.php', args, function (data) { // success handler
+    $('#lista').empty();
+    if(data == null){
+      $('#lista').append('<li>'+'No se encontraron resultados'+'</li>');
+    }else{
+      $.each(data, function(key, value) {
+        var path2file = encodeURI(value+"/"+key);
+        $('#lista').append('<li><a href='+path2file+'>'+key+'</a></li>');
+      });
+    }
   });
 }
 function rellenarSelects(){
-  $.ajax({
-    type: 'GET',
-    url: 'tagger.php',
-    data: '',
-    dataType: 'json',
-    success: function (data) {
-      $.each(data, function(index, element) {
-        switch (element) {
-          case 'grado':
-            $('#grado').append(new Option(index, index));
-            break;
-          case 'curso':
-            $('#curso').append(new Option(index, index));
-            break;
-          case 'asig':
-            $('#asig').append(new Option(index, index));
-            break;
-          case 'anio':
-            $('#anio').append(new Option(index, index));
-            break;
-        }
-      });
-    }
+  $.getJSON( 'tagger.php', function (data) {  // success handler
+    $.each(data, function(index, element) {
+      switch (element) {
+        case 'grado':
+          $('#grado').append(new Option(index, index));
+          break;
+        case 'curso':
+          $('#curso').append(new Option(index, index));
+          break;
+        case 'asig':
+          $('#asig').append(new Option(index, index));
+          break;
+        case 'anio':
+          $('#anio').append(new Option(index, index));
+          break;
+      }
+    });
   });
 }
 $(document).ready(function(){
