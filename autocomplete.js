7b29b73678d24ec39cc361fbdcf3582d290435c5
xehartnort <xehartnort@gmail.com>
2016-11-09 17:24:05 +0100

    pequeñños bugfixes

--- autocomplete.js
+++ autocomplete.js
@@ -1,19 +1,11 @@
 function mostrarResultados(){
-  /*var array_tags = split( $( "#query" ).val() );
-  var args="";
-  for (i = 0; i < array_tags.length; i++) {
-    if(array_tags[i]!="")
-      args += encodeURI("tag"+i+"="+array_tags[i]);
-    if (i+1 < array_tags.length)// if not last iteration
-      args += "&";
-  }*/
   var args = "tag0="+encodeURI($("#grado").val())+
-      "&tag1="+encodeURI($("#asig").val())+
-      "&tag2="+encodeURI($("#anio").val())+
-      "&tag3="+encodeURI($("#curso").val());
+    "&tag1="+encodeURI($("#asig").val())+
+    "&tag2="+encodeURI($("#anio").val())+
+    "&tag3="+encodeURI($("#curso").val());
   $.getJSON( 'getter.php', args, function (data) { // success handler
     $('#lista').empty();
-    if(data == null){
+    if(data.length==0 || data==null){
       $('#lista').append('<li>'+'No se encontraron resultados'+'</li>');
     }else{
       $.each(data, function(key, value) {
@@ -23,41 +15,15 @@ function mostrarResultados(){
     }
   });
 }
-function split( val ) {
-  return val.split( /,\s*/ );
-}
-function extractLast( term ) {
-  return split( term ).pop();
-}
-
 function sugerencias( id_input ){
   var termTemplate = "<span class='ui-autocomplete-term'>%s</span>";
   $( "#"+id_input ).autocomplete({
     delay: 500,
-    autoFocus: true,
+    minLength: 2,
     source: function( request, response ) {
-      $.getJSON( "tagger.php",
-        /*extractLast( */"term="+request.term+"&"+"tipo="+id_input
-      , response );
+      var args = "term="+request.term+"&"+"tipo="+id_input;
+      $.getJSON( "tagger.php", args, response );
     },
-    search: function() {
-      // custom minLength
-      var term = extractLast( this.value );
-      if ( term.length < 2 ) {
-        return false;
-      }
-    },
-    /*select: function( event, ui ) {
-      var terms = split( this.value );
-      // remove the current input
-      terms.pop();
-      // add the selected item
-      terms.push( ui.item.value );
-      // add placeholder to get the comma-and-space at the end
-      terms.push( "" );
-      this.value = terms.join( ", " );
-      return false;
-    },*/
     open: function (e, ui) { // hace que la selección escrita sea en negrita
         var acData = $(this).data('ui-autocomplete');
         acData
@@ -75,15 +41,14 @@ function sugerencias( id_input ){
 $(document).ready(function() {
   $('#grado').val("");
   sugerencias("grado");
-  $('#grado').on('input', function() { mostrarResultados(); });
+  $('#grado').on('input', function(){mostrarResultados();});
   $('#curso').val("");
   sugerencias("curso");
-  $('#curso').on('input', function() { mostrarResultados(); });
+  $('#curso').on('input', function(){mostrarResultados();});
   $('#asig').val("");
   sugerencias("asig");
-  $('#asig').on('input', function() { mostrarResultados(); });
+  $('#asig').on('input', function(){mostrarResultados();});
   $('#anio').val("");
   sugerencias("anio");
-  $('#anio').on('input', function() { mostrarResultados(); });
-
+  $('#anio').on('input', function(){mostrarResultados();});
 });
--- getter.php
+++ getter.php
@@ -1,26 +1,23 @@
 <?php
-$db = new PDO("sqlite:examenes.db");
-$query_text = "";
-$result = [];
-$get_size = count($_GET);
-if( $get_size > 1 ){
-  for($i=0; $i<$get_size; ++$i) {
+if( $_GET["tag0"] !="" || $_GET["tag1"] !="" || $_GET["tag2"] !="" || $_GET["tag3"] !=""){
+  $db = new PDO("sqlite:examenes.db");
+  $result = [];
+  $query_text = "";
+  for($i=0; $i<4; ++$i) {
     $query_text .= "SELECT nom_doc, ruta_doc FROM examen WHERE nom_tag_id LIKE :tag".$i;
-    if($i+1 < $get_size){ // if not last iteration
+    if($i+1 < 4){ // if not last iteration
       $query_text .= " INTERSECT ";
     }
   }
-}else{
-  $query_text = "SELECT nom_doc, ruta_doc FROM examen WHERE nom_tag_id LIKE :tag0";
+  $query = $db->prepare($query_text);
+  foreach ($_GET as $key => $value) {
+    $query->bindValue(':'.$key, $value.'%', PDO::PARAM_STR);
+  }
+  $query->execute();
+  $results = $query->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($results as $row) {
+    $result[$row["nom_doc"]] = $row["ruta_doc"];
+  }
+  echo json_encode($result);
 }
-$query = $db->prepare($query_text);
-foreach ($_GET as $key => $value) {
-  $query->bindValue(':'.$key, $value.'%', PDO::PARAM_STR);
-}
-$query->execute();
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
-foreach ($results as $row) {
-  $result[$row["nom_doc"]] = $row["ruta_doc"];
-}
-echo json_encode($result);
 ?>
