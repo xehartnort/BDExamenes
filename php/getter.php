c8595ab331da9dcbb288d55677d79540cd665627
xehartnort <xehartnort@gmail.com>
2016-12-15 15:49:39 +0100

    para mayor coeherencia, los resultados se ordenan

--- php/getter.php
+++ php/getter.php
@@ -14,11 +14,13 @@ if( $_GET["tag0"] !="" || $_GET["tag1"] !="" || $_GET["tag2"] !="" || $_GET["tag
   $db = new PDO("sqlite:../examenes.db");
   $query_text = "";
   for($i=0; $i<4; ++$i) { # http://stackoverflow.com/questions/2621382/alternative-to-intersect-in-mysql
-    $query_text.="SELECT nom_doc, ruta_doc FROM examen WHERE tipo_tag=:ttag".$i." and nom_tag_id LIKE :tag".$i." and comprobado=1";
+    $query_text.="SELECT nom_doc, ruta_doc FROM examen WHERE tipo_tag=:ttag".$i;
+    $query_text.=" AND nom_tag_id LIKE :tag".$i." AND comprobado=1 ";
     if($i+1 < 4){ // if not last iteration
       $query_text .= " INTERSECT ";
     }
   }
+  $query_text.=" ORDER BY nom_doc ";
   $tipo_tags = array("grado", "asig", "anio", "curso");
   $tags = array("tag0", "tag1", "tag2", "tag3");
   $query = $db->prepare($query_text);
