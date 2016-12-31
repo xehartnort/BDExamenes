fce94317b8018d05d1996dd0a986da695de209c4
xehartnort <xehartnort@gmail.com>
2016-12-31 23:36:28 +0100

    mayor parte del procesamiento ocurre ahora en el cliente y no en el servidor

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -4,64 +4,57 @@ function mostrarResultados(numpag){
             "&tag2="+encodeURI($("#anio").val())+
             "&tag3="+encodeURI($("#curso").val())+
             "&page="+numpag;
-  $.getJSON( './php/getter.php', args, function (data) { // success handler
-    if(numpag==1){
-      $('#lista').empty();
-    }
-    if( data["num_r"] > 0){
-      $.each(data, function(key, value) {
-        if(key!="num_r"){
-          if(value != null){
-            var path2file = encodeURI(value+"/"+key);
-            $('#lista').append('<li><a href='+path2file+' target="_blank">'+key+'</a></li>');
+  $.getJSON( './php/getter.php', args, function (data, status) { // success handler
+    if(status == "success"){
+      if(numpag==1){
+        $("#lista").empty();
+      }
+      if( data["num_r"] > 0){
+        $.each(data, function(key, value) {
+          if(key!="num_r"){
+            if(value != null){
+              var path2file = encodeURI(value+"/"+key);
+              $('#lista').append('<li><a href='+path2file+' target="_blank">'+key+'</a></li>');
+            }
           }
-        }
-      });
-    }else{
-      $("#lista").append("<li><a>No se encontraron resultados</a></li>");
+        });
+      }else{
+        $("#lista").empty();
+        $("#lista").append("<li><a>No se encontraron resultados</a></li>");
+      }
     }
   });
 }
 
-function autocompletar( id_input ){
-  var args = "&caller="+id_input;
-  switch (id_input) {
-    case "grado":
-      args += "&tag0="+encodeURI($("#curso").val())+"&tag1="+encodeURI($("#asig").val())+
-              "&tag2="+encodeURI($("#anio").val());
-      break;
-    case "asig":
-      args += "&tag0="+encodeURI($("#curso").val())+"&tag1="+encodeURI($("#grado").val())+
-              "&tag2="+encodeURI($("#anio").val());
-      break;
-    case "anio":
-      args += "&tag0="+encodeURI($("#curso").val())+"&tag1="+encodeURI($("#asig").val())+
-              "&tag2="+encodeURI($("#grado").val());
-      break;
-    case "curso":
-      args += "&tag0="+encodeURI($("#grado").val())+"&tag1="+encodeURI($("#asig").val())+
-              "&tag2="+encodeURI($("#anio").val());
-      break;
-  }
+function autocompletar( id_input, lcache ){
   $( "#"+id_input ).autocomplete({
+    autoFocus: true,
+    minLength: 0,
+    create: function( event, ui ) {
+      $( '#'+id_input ).val("");
+      $.getJSON( "./php/tagger.php", "caller="+id_input, 
+        function(data, status){
+          if(status=="success"){
+            lcache = data;
+          }
+        }
+      );
+    },
     source: function( request, response ) {
-      $.getJSON( "./php/tagger.php", "term="+request.term+args, response );
+      cache = lcache;
+      var escapedString = $.ui.autocomplete.escapeRegex( request.term );
+      var matcher = new RegExp( "^" + escapedString.replace(/[aeiouáéíóú]/gi, '[aeiouáéíóú]'), "i" );
+      response( $.grep( cache, function( item ){
+          return matcher.test( item );
+        })
+      );
+    },
+    change: function(){
+      setTimeout(function(){mostrarResultados(1);},50); // sin timeout no toma el último valor
     },
     select: function(){
       setTimeout(function(){mostrarResultados(1);},50); // sin timeout no toma el último valor
-    },
-    open: function (e, ui) { // hace que la selección escrita sea en negrita
-        var acData = $(this).data('ui-autocomplete');
-        acData
-        .menu
-        .element
-        .find('li')
-        .each(function () {
-            var me = $(this);
-            var keywords = acData.term.split(' ').join('|');
-            me.html(me.text().replace(new RegExp("(" + keywords + ")", "gi"), '<b>$1</b>'));
-         });
-     }
+    }
   });
 }
 
@@ -69,33 +62,14 @@ $(document).ready(function() {
   $("button#up").hide();
   $("#lista").append("<li><a>Los resultados se mostrarán aquí</a></li>");
 
-  $('#grado').val("");
-  $('#grado').on( "autocompleteselect", function( event, ui ){});
-  $('#grado').on('input', function(){
-    autocompletar("grado");
-    mostrarResultados(1);
-  });
-
-  $('#asig').val("");
-  $('#asig').on( "autocompleteselect", function( event, ui ){});
-  $('#asig').on('input', function(){
-    autocompletar("asig");
-    mostrarResultados(1);
-  });
-
-  $('#anio').val("");
-  $('#anio').on( "autocompleteselect", function( event, ui ){});
-  $('#anio').on('input', function(){
-    autocompletar("anio");
-    mostrarResultados(1);
-  });
-
-  $('#curso').val("");
-  $('#curso').on( "autocompleteselect", function( event, ui ){});
-  $('#curso').on('input', function(){
-    autocompletar("curso");
-    mostrarResultados(1);
-  });
+  var cache_grado;
+  var cache_asig;
+  var cache_anio;
+  var cache_curso;
+  autocompletar("grado", cache_grado);
+  autocompletar("asig", cache_asig);
+  autocompletar("anio", cache_anio);
+  autocompletar("curso", cache_curso);
 
   $("button#up").on('click',function(){ 
     $("html, body").animate({ scrollTop: 0 }, "slow");
--- php/tagger.php
+++ php/tagger.php
@@ -1,27 +1,7 @@
 <?php
-//  $_GET["tag0"]="";
-//  $_GET["tag1"]="";
-//  $_GET["tag2"]="";
-//  $_GET["term"]="algoritmica";
-//  $_GET["caller"]="asig";
 $db = new PDO("sqlite:../examenes.db");
-$tildes=array('a','e','i','o','u');
-$sin_tildes=array('_','_','_','_','_');
-$_GET["term"]=str_replace($tildes, $sin_tildes, $_GET["term"]);
-$query_text = "select nom_tag from etiqueta where nom_tag like :term and tipo_tag=:caller and id_doc_id in (";
-for($i=0; $i<3; $i++){
-   $query_text .= "select id_doc_id from doctag where nom_tag_id like :tag".$i;
-   if($i+1 < 3){ // if not last iteration
-     $query_text .= " INTERSECT ";
-   }else{
-     $query_text .= ") group by nom_tag";
-   }
-}
+$query_text = "SELECT nom_tag FROM etiqueta WHERE tipo_tag=:caller GROUP BY nom_tag";
 $query = $db->prepare($query_text);
-for($i=0; $i<3; ++$i) {
-  $query->bindValue(':tag'.$i, $_GET["tag".$i].'%', PDO::PARAM_STR);
-}
-$query->bindValue(':term', $_GET["term"].'%', PDO::PARAM_STR);
 $query->bindValue(':caller', $_GET["caller"], PDO::PARAM_STR);
 $query->execute();
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
