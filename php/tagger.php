4914741c1b92123553eee56c3695477b4154f169
Daniel Jiménez López <xehartnort@gmail.com>
2017-07-03 22:02:56 +0200

    Update tagger.php
    Se muestran también asignaturas que no tengan algún examen como sugerencia

--- php/tagger.php
+++ php/tagger.php
@@ -1,11 +1,11 @@
 <?php
 $db = new PDO("sqlite:../examenes.db");
-$query_text = "SELECT nom_tag_id FROM examen WHERE tipo_tag='asig' OR tipo_tag='grado' GROUP BY nom_tag_id ORDER BY preferencia DESC, LENGTH(nom_tag_id)";
+$query_text = "SELECT nom_tag FROM tag WHERE tipo_tag!='curso' GROUP BY nom_tag ORDER BY preferencia DESC, LENGTH(nom_tag)";
 $query = $db->prepare($query_text);
 $query->execute();
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 foreach ($results as $row) {
-	$result[] = $row["nom_tag_id"];
+	$result[] = $row["nom_tag"];
 }
 echo json_encode($result);
 ?>
