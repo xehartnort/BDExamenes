179ce9d181761c74fa847770643929198e35634b
xehartnort <xehartnort@debian>
2018-11-07 23:38:02 +0100

    nuevas miniaturas generadas

--- dbtools/thumbailGen.py
+++ dbtools/thumbailGen.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python -OO
 # -*- coding: utf-8 -*-
 
 import subprocess
@@ -11,7 +10,7 @@ def hash_file(path, block_size=4096):
         for chunk in iter(lambda: f.read(block_size), b''):
             hash_string.update(chunk)
     return hash_string.hexdigest()
-
+icons = os.listdir("../img")
 for (dirpath, dirnames, files) in os.walk("../exámenes"):
     for filename in files:
         fpath = None
@@ -21,4 +20,5 @@ for (dirpath, dirnames, files) in os.walk("../exámenes"):
         	fpath = dirpath+"/"+filename
         if( fpath != None):
         	md5 = hash_file(dirpath+"/"+filename)
-        	subprocess.call(['gm', 'convert','-sampling-factor', '4:2:0', '-colorspace', 'RGB', '-strip', '-crop', '100x50%', '-resize', '315x384','-quality','50', fpath, "../img/"+md5+".jpg"])
+                if not str(md5)+".jpg" in icons:
+        	        subprocess.call(['gm', 'convert','-sampling-factor', '4:2:0', '-colorspace', 'RGB', '-strip', '-crop', '100x50%', '-resize', '315x384','-quality','50', fpath, "../img/"+md5+".jpg"])
